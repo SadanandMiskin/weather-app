661d4477903b1888d8893f83efd9895df92a5b5b
Sadanand Miskin <miskinsadanand@gmail.com>
2023-12-17 23:18:06 +0530

    Update setProxy.js

--- src/setProxy.js
+++ src/setProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use('/api', createProxyMiddleware({
-        target: 'http://weather-app-sadanandmiskin.vercel.app/', // Your backend URL
+        target: 'http://weather-app-sadanandmiskin.vercel.app/', 
         changeOrigin: true,
     }));
 };
